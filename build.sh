285af95371b4c3ea7726ee15b69f4fe6afb46afc
XePeleato <edu@error404software.com>
2016-08-31 02:26:34 +0200

    Fix the defconfig name in the build script * I copied it from my P9 Lite repo and forgot to change it

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ export PATH="$PATH:/home/xepeleato/CM13/prebuilts/gcc/linux-x86/aarch64/aarch64-
 export CROSS_COMPILE="aarch64-linux-android-"
 mkdir -p out
 echo "Paths and Toolchain loaded!"
-make ARCH=arm64 O=out merge_hi6250_defconfig
+make ARCH=arm64 O=out hisi_hi6210sft_defconfig
 make ARCH=arm64 O=out -j8
 printf "\nDone! if it compiled correctly, you'll find the compiled Image at ../out/arch/arm64/boot/Image"
 printf "\nThe modules are at out/(device)/*.ko\n"
